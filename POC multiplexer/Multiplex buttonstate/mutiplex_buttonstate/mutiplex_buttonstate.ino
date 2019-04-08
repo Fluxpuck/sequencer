741635ba5948ff611b74edc41fad5ecf622cc59c
Znizzel <47626215+Znizzel@users.noreply.github.com>
2019-04-08 16:03:43 +0200

    Buttonstate rework
    door een onopgemerkte bug waarbij de loop de huidige buttonState één keer oversloeg kwam het aansturen van de leds niet goed uit (++1 werd eerder uitgevoerd dus de leds werden per button één doorgeschoven). Deze bug is nu verholpen door de debounce eruit te halen.

--- POC multiplexer/Multiplex buttonstate/mutiplex_buttonstate/mutiplex_buttonstate.ino
+++ POC multiplexer/Multiplex buttonstate/mutiplex_buttonstate/mutiplex_buttonstate.ino
@@ -2,8 +2,7 @@
 MUX74HC4067 mux(7, 8, 9, 10, 11);
 
 int state = HIGH;
-int reading;           // the current reading from the input pin
-int previous = LOW;    // the previous reading from the input pin
+int state1 = HIGH;
 long time = 0;         // the last time the output pin was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
 
@@ -19,7 +18,7 @@ void loop()
 {
   byte data;
 
-  for (byte i = 0; i < 1; ++i) // < X, X = amount of channels
+  for (byte i = 0; i < 2; ++i) // < X, X = amount of channels
   {
     data = mux.read(i);
 
@@ -27,29 +26,35 @@ void loop()
     Serial.print(i);
     Serial.print(" is ");
     if ( data == HIGH ) Serial.println("not pressed");
-    else Serial.println("pressed ");
-
-    reading = data;
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (state == HIGH) {
+    else if (i == 0 && state == HIGH) {
+      Serial.println("pressed ");
       state = LOW;
       Serial.print("button ");
-      Serial.print(i);
+      Serial.print(0);
       Serial.println(" = on");
     }
-    else {
+    else if (i == 0 && state == LOW) {
+      Serial.println("pressed ");
       state = HIGH;
       Serial.print("button ");
-      Serial.print(i);
+      Serial.print(0);
+      Serial.println(" = off");
+        }
+    else if (i == 1 && state1 == HIGH){
+      Serial.println("pressed ");
+      state1 = LOW;
+      Serial.print("button ");
+      Serial.print(1);
+      Serial.println(" = on");
+    }
+    else if (i == 1 && state1 == LOW){
+      Serial.println("pressed ");
+      state1 = HIGH;
+      Serial.print("button ");
+      Serial.print(1);
       Serial.println(" = off");
     }
-
-    time = millis();    
-  }
-  previous = reading;
+        
 }
     
   
