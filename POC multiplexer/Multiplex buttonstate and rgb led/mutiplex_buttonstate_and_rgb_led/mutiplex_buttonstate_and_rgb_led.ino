5eb5fbc34496f69ce88014b30e93e3512ff17107
Znizzel <47626215+Znizzel@users.noreply.github.com>
2019-04-09 09:54:37 +0200

    Fixed buttonState en led's
    Laatste keer was het aanzetten van de LED's telkens één loop telaat waardoor de  knoppen de verkeerde LED's aanzette. Met deze nieuwe versie wordt er gewerkt met de nieuwe ButtonStates en zonder de debouncer op 'n button push. Alle knoppen sturen nu de juiste LED's aan.

--- POC multiplexer/Multiplex buttonstate and rgb led/mutiplex_buttonstate_and_rgb_led/mutiplex_buttonstate_and_rgb_led.ino
+++ POC multiplexer/Multiplex buttonstate and rgb led/mutiplex_buttonstate_and_rgb_led/mutiplex_buttonstate_and_rgb_led.ino
@@ -2,32 +2,49 @@
 MUX74HC4067 mux(7, 8, 9, 10, 11);
 
 int state = HIGH;
-int reading;           // the current reading from the input pin
-int previous = LOW;    // the previous reading from the input pin
-long time = 0;         // the last time the output pin was toggled
-long debounce = 200;   // the debounce time, increase if the output flickers
+int state1 = HIGH;
+int state2 = HIGH;
+int state3 = HIGH;
 
 int redPin = 4;
 int greenPin = 5;
 int bluePin = 6;
+int redPin1 = 50;
+int greenPin1 = 51;
+int bluePin1 = 52;
+int redPin2 = 49;
+int greenPin2 = 48;
+int bluePin2 = 47;
+int redPin3 = 46;
+int greenPin3 = 45;
+int bluePin3 = 44;
 
 void setup()
 {
   Serial.begin(9600);  // Initializes serial port
-    while ( !Serial ) ;
+  while ( !Serial ) ;
   
   mux.signalPin(3, INPUT, DIGITAL);
-  
+
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+  pinMode(redPin1, OUTPUT);
+  pinMode(greenPin1, OUTPUT);
+  pinMode(bluePin1, OUTPUT);
+  pinMode(redPin2, OUTPUT);
+  pinMode(greenPin2, OUTPUT);
+  pinMode(bluePin2, OUTPUT);
+  pinMode(redPin3, OUTPUT);
+  pinMode(greenPin3, OUTPUT);
+  pinMode(bluePin3, OUTPUT); 
 }
 
 void loop()
 {
   byte data;
 
-  for (byte i = 0; i < 1; ++i) // < X, X = amount of channels
+  for (byte i = 0; i < 4; ++i) // < X, X = amount of channels
   {
     data = mux.read(i);
 
@@ -35,39 +52,73 @@ void loop()
     Serial.print(i);
     Serial.print(" is ");
     if ( data == HIGH ) Serial.println("not pressed");
-    else Serial.println("pressed ");
-
-    reading = data;
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (state == HIGH) {
+    else if (i == 0 && state == HIGH) {
+      Serial.println("pressed ");
       state = LOW;
-      Serial.print("button");
-      Serial.print(i);
-      Serial.print(" = on");
-
-      setColor(0, 0, 255);
-      Serial.println(" with red led on"); // blue
+      setColor(255, 0, 0); // blue
+      Serial.print("button ");
+      Serial.print(0);
+      Serial.println(" = on");
     }
-    else {
+    else if (i == 0 && state == LOW) {
+      Serial.println("pressed ");
       state = HIGH;
-      Serial.print("button");
-      Serial.print(i);
-      Serial.print(" = off");
-
-      setColor(0, 0, 0);
-      Serial.println(" with red led off"); // offline
+      setColor(0, 0, 0); // off
+      Serial.print("button ");
+      Serial.print(0);
+      Serial.println(" = off");
+        }
+    else if (i == 1 && state1 == HIGH){
+      Serial.println("pressed ");
+      state1 = LOW;
+      setColor1(255, 0, 0); // red
+      Serial.print("button ");
+      Serial.print(1);
+      Serial.println(" = on");
     }
-
-    time = millis();    
-  }
-  previous = reading;
+    else if (i == 1 && state1 == LOW){
+      Serial.println("pressed ");
+      state1 = HIGH;
+      setColor1(0, 0, 0); // off
+      Serial.print("button ");
+      Serial.print(1);
+      Serial.println(" = off");
+    }
+    else if (i == 2 && state2 == HIGH){
+      Serial.println("pressed ");
+      state2 = LOW;
+      setColor2(255, 0, 0); // red
+      Serial.print("button ");
+      Serial.print(2);
+      Serial.println(" = on");
+    }
+    else if (i == 2 && state2 == LOW){
+      Serial.println("pressed ");
+      state2 = HIGH;
+      setColor2(0, 0, 0); // off
+      Serial.print("button ");
+      Serial.print(2);
+      Serial.println(" = off");
+    }
+    else if (i == 3 && state3 == HIGH){
+      Serial.println("pressed ");
+      state3 = LOW;
+      setColor3(255, 0, 0); // red
+      Serial.print("button ");
+      Serial.print(3);
+      Serial.println(" = on");
+    }
+    else if (i == 3 && state3 == LOW){
+      Serial.println("pressed ");
+      state3 = HIGH;
+      setColor3(0, 0, 0); // off
+      Serial.print("button ");
+      Serial.print(3);
+      Serial.println(" = off");
+    }
+        
 }
-    
-  
-  delay(1500);
+delay(125);
 }
 
 void setColor(int red, int green, int blue)
@@ -75,4 +126,24 @@ void setColor(int red, int green, int blue)
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
+}
+void setColor1(int red, int green, int blue)
+{
+  digitalWrite(redPin1, red);
+  digitalWrite(greenPin1, green);
+  digitalWrite(bluePin1, blue);  
+}
+
+void setColor2(int red, int green, int blue)
+{
+  digitalWrite(redPin2, red);
+  digitalWrite(greenPin2, green);
+  digitalWrite(bluePin2, blue);
+}
+
+void setColor3(int red, int green, int blue)
+{
+  digitalWrite(redPin3, red);
+  digitalWrite(greenPin3, green);
+  digitalWrite(bluePin3, blue);
 }
