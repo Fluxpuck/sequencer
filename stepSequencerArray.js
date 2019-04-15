81f5bad3081f7c7a54ac345a29d0c56b76613cf9
WouterWijnands <w.wijnands@student.fontys.nl>
2019-04-15 11:12:00 +0200

    Stes toegevoegd aan sequencer

--- stepSequencerArray.js
+++ stepSequencerArray.js
@@ -6,66 +6,58 @@ var StepSequencer = require('step-sequencer');
 
 var tempo = 60;
 var division = 8;
-var sequence_arduino = [0,1,0,0,0,0,0,1];
+var sequence_arduino = [0,1,2,3,4,5,6,7];
 var stepSequencer_arduino = new StepSequencer(tempo, division, sequence_arduino);
 
-// listen for the "keypress" event
-// process.stdin.on('keypress', function (ch, key) {
-//     if (key.name == 'p') 
-//     { 
-//         player.play('/sounds/kick.wav', (err) => {
-//             if (err) console.log(`Could not play sound: ${err}`);
-//         });
-//     }
-//   });
-//   process.stdin.setRawMode(true);
-
-for(var i=0; i++;i <sequence_arduino.length){
-    if(i==1){
-        console.log('Kick');
-        player.play('sounds/kick.wav', (err) => {
-            if (err) console.log(`Could not play sound: ${err}`);
-        });
-    };
-};
-
-stepSequencer_arduino.play();
-
-// stepSequencer_0.on('0', function (step) {
-
-//     player.play('sounds/kick.wav', (err) => {
-//         if (err) console.log(`Could not play sound: ${err}`);
-//     });
-
-//     console.log("KICK")
-//     // console.log(step);
-// })
-//     .on('1', function (step) {
-//         console.log(step);
-//     })
-//     .on('2', function (step) {
-//         console.log(step);
-//     })
-//     .on('3', function (step) {
-//         console.log(step);
-//     })
-//     .on('4', function (step) {
-
+// for(var i=0; i++;i <sequence_arduino.length){
+//     if(i==1){
+//         console.log('Kick');
 //         player.play('sounds/kick.wav', (err) => {
 //             if (err) console.log(`Could not play sound: ${err}`);
 //         });
+//     };
+// };
 
-//         console.log("KICK")
-//         // console.log(step);
-//     })
-//     .on('5', function (step) {
-//         console.log(step);
-//     })
-//     .on('6', function (step) {
-//         console.log(step);
-//     })
-//     .on('7', function (step) {
-//         console.log(step);
-//     });
+// stepSequencer_arduino.play();
 
-// stepSequencer_0.play();
+    stepSequencer_arduino.on('0', function (step) {
+
+    })
+    .on('1', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('2', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('3', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('4', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('5', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('6', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    .on('7', function (step) {
+        player.play('sounds/kick.wav', (err) => {
+            if (err) console.log(`Could not play sound: ${err}`);
+        });
+    })
+    
+
+stepSequencer_arduino.play();
